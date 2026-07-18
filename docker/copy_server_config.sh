47c2db3fb35e1eacdc8381991489b570b76a5817
Jacob Pelletier <OfficialUnrealNetwork@users.noreply.github.com>
2026-07-18 00:37:30 -0400

    Load persistent FS25 configuration script

--- docker/copy_server_config.sh
+++ docker/copy_server_config.sh
@@ -1,30 +1,30 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+set -euo pipefail
 
+WINEPREFIX="${WINEPREFIX:-/home/container/.fs25server}"
 TEMPLATE_DIR="/home/nobody/.build/fs25"
-RUNTIME_USER="${USER:-nobody}"
 
-GAME_DIR="${HOME}/.fs25server/drive_c/Program Files (x86)/Farming Simulator 2025"
-PROFILE_DIR="${HOME}/.fs25server/drive_c/users/${RUNTIME_USER}/Documents/My Games/FarmingSimulator2025/dedicated_server"
+GAME_DIR="${WINEPREFIX}/drive_c/Program Files (x86)/Farming Simulator 2025"
+PROFILE_DIR="${WINEPREFIX}/drive_c/users/nobody/Documents/My Games/FarmingSimulator2025/dedicated_server"
 
 GAME_CONFIG="${GAME_DIR}/dedicatedServer.xml"
 SERVER_CONFIG="${PROFILE_DIR}/dedicatedServerConfig.xml"
 
-if [ ! -d "${GAME_DIR}" ]; then
-    echo "ERROR: Farming Simulator 25 is not installed."
+if [[ ! -d "${GAME_DIR}" ]]; then
+    echo "ERROR: Farming Simulator 25 installation was not found."
     exit 1
 fi
 
 mkdir -p "${PROFILE_DIR}"
 
-if [ ! -s "${GAME_CONFIG}" ]; then
+if [[ ! -s "${GAME_CONFIG}" ]]; then
     echo "Creating initial dedicatedServer.xml"
     cp "${TEMPLATE_DIR}/default_dedicatedServer.xml" "${GAME_CONFIG}"
 else
     echo "Preserving existing dedicatedServer.xml"
 fi
 
-if [ ! -s "${SERVER_CONFIG}" ]; then
+if [[ ! -s "${SERVER_CONFIG}" ]]; then
     echo "Creating initial dedicatedServerConfig.xml"
     cp "${TEMPLATE_DIR}/default_dedicatedServerConfig.xml" "${SERVER_CONFIG}"
 else
